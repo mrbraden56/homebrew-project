80913f1e69d1b108224bbd98907c1b3943c85558
Braden Lockwood <braden.lockwood@kiewit.com>
2024-03-10 09:39:11 -0500

    changed release artificat to 19

--- Formula/Project.rb
+++ Formula/Project.rb
@@ -1,7 +1,7 @@
 class Project < Formula
   desc "Description"
   homepage "https://github.com/mrbraden56/project"
-  url "https://github.com/mrbraden56/project/releases/download/18/project-v18.zip"
+  url "https://github.com/mrbraden56/project/releases/download/18/project-v19.zip"
   sha256 "a0db65ffc79d07b4ceefe9cef596f57029eca2d47059cea5ad91d124b7e9884f"
   license ""
 
