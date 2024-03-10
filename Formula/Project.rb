6f487a06a365e802e1546b99fcbe95b00e82441f
Braden Lockwood <braden.lockwood@kiewit.com>
2024-03-10 14:14:25 -0500

    release v22

--- Formula/Project.rb
+++ Formula/Project.rb
@@ -1,8 +1,8 @@
 class Project < Formula
   desc "Description"
   homepage "https://github.com/mrbraden56/project"
-  url "https://github.com/mrbraden56/project/releases/download/20/project-v20.zip"
-  sha256 "b6773d82226b2ad49929a00391257363550ecbf330ea2e318339dffd365cf851"
+  url "https://github.com/mrbraden56/project/releases/download/22/project-v22.zip"
+  sha256 "24bf9a9698f1f3d6795015f943beb99526175192f1f9c75c81043f686bd2a15c"
   license ""
 
   depends_on "go" => :build
