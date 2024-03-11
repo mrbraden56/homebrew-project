bf1298ec9468ca30e5d6797b4f8197aad3b2a407
Braden Lockwood <braden.lockwood@kiewit.com>
2024-03-10 20:10:39 -0500

    edited formula

--- Formula/Project.rb
+++ Formula/Project.rb
@@ -1,16 +1,14 @@
 class Project < Formula
   desc "Description"
   homepage "https://github.com/mrbraden56/project"
-  url "https://github.com/mrbraden56/project/releases/download/26/project-v26.zip"
-  sha256 "d8bb766d8ffbb2a42db9ea022459adc90710d3fac0c7dbfd6f096c63338961b5"
-  license ""
 
-  depends_on "go" => :build
+  on_macos do
+    url "https://github.com/mrbraden56/project/releases/download/26/project-v26.zip"
+    sha256 "d8bb766d8ffbb2a42db9ea022459adc90710d3fac0c7dbfd6f096c63338961b5"
 
-  def install
-    ENV["GO111MODULE"] = "on"
-    system "go", "build", *std_go_args
-    bin.install "project"
+    def install
+      bin.install "project"
+    end
   end
 
   test do
